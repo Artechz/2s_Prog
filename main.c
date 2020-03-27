31df27af22e09fb0c4afecbbcde2c719c392c1ff
Artechz <arnau.sanz@gmail.com>
2020-03-27 13:49:49 +0100

    Added all UI text and images. UI is static (not dynamic at the moment); needs to implement buttons too.

--- main.c
+++ main.c
@@ -14,9 +14,10 @@
 #include "LS_allegro.h"
 #define MAXSTRING 26
 #define WIN_TITLE "LS Racing by Arnau Sanz & Josep Segarra"
-#define WIN_WIDTH 900
+#define WIN_WIDTH 1000
 #define WIN_HEIGHT 600
-#define BACKGROUND_FILE "/files/boxes.png"
+#define BACKGROUND_IMAGE "files/boxes.png"
+#define TIRES_IMAGE "files/neumaticos.png"
 
 typedef struct {
     char driverName[MAXSTRING];
@@ -78,8 +79,13 @@ int main (void) {
     int i, optionDone = 0, exit = 0, allegroExit = 0;
     //char aux;
     ALLEGRO_BITMAP * background = NULL;
+    ALLEGRO_BITMAP * tires = NULL;
+
+    int background_color = BLACK;    //TODO
+    int text_color = WHITE;
 
     do {
+
         printf("\nWelcome to LS Racing\n\n");
         printf("\t 1. Configure Car\n");
         printf("\t 2. Race\n");
@@ -120,26 +126,84 @@ int main (void) {
 
                 LS_allegro_init(WIN_WIDTH, WIN_HEIGHT, WIN_TITLE);
 
-                background = al_load_bitmap(BACKGROUND_FILE);
+                printf("\nPath: %s", al_get_current_directory());                                                       //DEBUG
+
+                //Loading the image as a bitmap, checking it loaded properly and saving its size for future scaling.
+                background = al_load_bitmap(BACKGROUND_IMAGE);
                 if (!background) {
                     printf("Failed to load background bitmap!\n");
                 }
 
-                //Bucle infinit del joc
+                const int background_width = al_get_bitmap_width(background);
+                const int background_height = al_get_bitmap_height(background);
+
+                tires = al_load_bitmap(TIRES_IMAGE);
+                if (!tires) {
+                    printf("Failed to load tires bitmap!\n");
+                }
+
+                const int tires_width = al_get_bitmap_width(tires);
+                const int tires_height = al_get_bitmap_height(tires);
+                const int tires_width_scaled =  al_get_bitmap_width(tires)/(WIN_WIDTH/200);
+                const int tires_height_scaled =  al_get_bitmap_height(tires)/(WIN_WIDTH/200);
+                const int tires_margin = tires_height_scaled/5;
+
+                //Infinite loop until ESC is pressed.
                 while (!allegroExit) {
 
-                    //Escoltem el teclat esperant la tecla ESC
+                    //Checking if ESC is pressed.
                     if (LS_allegro_key_pressed(ALLEGRO_KEY_ESCAPE)) {
                         allegroExit = 1;
                     }
 
-                    al_draw_bitmap(background,0,0,0);
+                    //Scales the boxes image to a size that fits the actual window.
+                    al_draw_scaled_bitmap (background, 0, 0, background_width, background_height, 0, 0, (WIN_WIDTH/5)*3, WIN_HEIGHT, 0);
+                    //Scaled the tires image so it's not too big for the arrows to have space.
+                    al_draw_scaled_bitmap (tires, 0, 0, tires_width, tires_height, WIN_WIDTH/5*3.4, WIN_HEIGHT/5, tires_width_scaled, tires_height_scaled, 0);
 
-                    //Donem l'ordre d'escriure el text de benvinguda
-                    al_draw_textf(LS_allegro_get_font(NORMAL),LS_allegro_get_color(WHITE),140,100,0,"%s","Heyy! Prem ESC per sortir...");
 
-                    //Pintem la pantalla de la finestra gràfica
-                    LS_allegro_clear_and_paint(BLACK);
+                    //Printing arrows
+                    //LEFT ARROW
+                    al_draw_line ( WIN_WIDTH/5*3.4 - tires_margin, WIN_HEIGHT/5 + tires_height_scaled/2, WIN_WIDTH/5*3.4 - 20 - tires_margin,
+                            WIN_HEIGHT/5 + tires_height_scaled/2, LS_allegro_get_color(text_color), WIN_HEIGHT/50);
+                    al_draw_filled_triangle ( WIN_WIDTH/5*3.4 - 40 - tires_margin, WIN_HEIGHT/5 + tires_height_scaled/2, WIN_WIDTH/5*3.4 - 20 - tires_margin,
+                            WIN_HEIGHT/5 + 15 + tires_height_scaled/2, WIN_WIDTH/5*3.4 - 20 - tires_margin, WIN_HEIGHT/5-15 + tires_height_scaled/2, LS_allegro_get_color(text_color));
+                    //RIGHT ARROW
+                    al_draw_line ( WIN_WIDTH/5*3.4 + tires_margin + tires_width_scaled, WIN_HEIGHT/5 + tires_height_scaled/2, WIN_WIDTH/5*3.4 + 20 + tires_width_scaled + tires_margin,
+                                   WIN_HEIGHT/5 + tires_height_scaled/2, LS_allegro_get_color(text_color), WIN_HEIGHT/50);
+                    al_draw_filled_triangle ( WIN_WIDTH/5*3.4 + 40 + tires_width_scaled + tires_margin, WIN_HEIGHT/5 + tires_height_scaled/2, WIN_WIDTH/5*3.4 + 20 + tires_width_scaled + tires_margin,
+                                              WIN_HEIGHT/5 + 15 + tires_height_scaled/2, WIN_WIDTH/5*3.4 + 20 + tires_width_scaled + tires_margin, WIN_HEIGHT/5 - 15 + tires_height_scaled/2, LS_allegro_get_color(text_color));
+                    //UP ARROW
+                    al_draw_line ( WIN_WIDTH/5*3.4 + tires_width_scaled/2, WIN_HEIGHT/5 - tires_margin, WIN_WIDTH/5*3.4 + tires_width_scaled/2,
+                                   WIN_HEIGHT/5 - 20 - tires_margin, LS_allegro_get_color(text_color), WIN_HEIGHT/50);
+                    al_draw_filled_triangle ( WIN_WIDTH/5*3.4 + tires_width_scaled/2, WIN_HEIGHT/5 - 40 - tires_margin, WIN_WIDTH/5*3.4 - 15 + tires_width_scaled/2,
+                                              WIN_HEIGHT/5 - 20 - tires_margin, WIN_WIDTH/5*3.4 + 15 + tires_width_scaled/2, WIN_HEIGHT/5 - 20 - tires_margin, LS_allegro_get_color(text_color));
+                    //DOWN ARROW
+                    al_draw_line ( WIN_WIDTH/5*3.4 + tires_width_scaled/2, WIN_HEIGHT/5 + tires_margin + tires_height_scaled, WIN_WIDTH/5*3.4 + tires_width_scaled/2,
+                                   WIN_HEIGHT/5 + 20 + tires_margin + tires_height_scaled, LS_allegro_get_color(text_color), WIN_HEIGHT/50);
+                    al_draw_filled_triangle ( WIN_WIDTH/5*3.4 + tires_width_scaled/2, WIN_HEIGHT/5 + 40 + tires_margin + tires_height_scaled, WIN_WIDTH/5*3.4 - 15 + tires_width_scaled/2,
+                                              WIN_HEIGHT/5 + 20 + tires_margin + tires_height_scaled, WIN_WIDTH/5*3.4 + 15 + tires_width_scaled/2, WIN_HEIGHT/5 + 20 + tires_margin + tires_height_scaled, LS_allegro_get_color(text_color));
+
+                    //Printing text
+                    al_draw_textf (LS_allegro_get_font(LARGE), LS_allegro_get_color(text_color), (WIN_WIDTH/4)*3,WIN_HEIGHT/5 - 80,0,"%s","TIRES");
+                    al_draw_textf (LS_allegro_get_font(SMALL), LS_allegro_get_color(text_color), (WIN_WIDTH/6)*5,WIN_HEIGHT/5 - 20,0,"%s","C5");
+                    al_draw_textf (LS_allegro_get_font(SMALL), LS_allegro_get_color(text_color), (WIN_WIDTH/6)*5,WIN_HEIGHT/5,0,"%s%d","SPEED: ", 5);
+                    al_draw_textf (LS_allegro_get_font(SMALL), LS_allegro_get_color(text_color), (WIN_WIDTH/6)*5,WIN_HEIGHT/5 + 20,0,"%s%d","ACCELERATION: ", 5);
+                    al_draw_textf (LS_allegro_get_font(SMALL), LS_allegro_get_color(text_color), (WIN_WIDTH/6)*5,WIN_HEIGHT/5 + 40,0,"%s%d","CONSUMPTION: ", -2);
+                    al_draw_textf (LS_allegro_get_font(SMALL), LS_allegro_get_color(text_color), (WIN_WIDTH/6)*5,WIN_HEIGHT/5 + 60,0,"%s%d","RELIABILITY: ", 2);
+
+                    al_draw_textf(LS_allegro_get_font(NORMAL), LS_allegro_get_color(text_color), WIN_WIDTH/5*3.2, WIN_HEIGHT/5*3, 0, "%s", "CURRENT CONFIGURATION");
+                    al_draw_textf(LS_allegro_get_font(SMALL), LS_allegro_get_color(text_color), WIN_WIDTH/5*3.2, WIN_HEIGHT/5*3 + 40, 0, "%s%s", "TIRES: ", "C5");
+                    al_draw_textf(LS_allegro_get_font(SMALL), LS_allegro_get_color(text_color), WIN_WIDTH/5*3.2, WIN_HEIGHT/5*3 + 60, 0, "%s%s", "AERO FRONT: ", "LOW LOAD");
+                    al_draw_textf(LS_allegro_get_font(SMALL), LS_allegro_get_color(text_color), WIN_WIDTH/5*3.2, WIN_HEIGHT/5*3 + 80, 0, "%s%s", "AERO MID: ", "MID LOAD");
+                    al_draw_textf(LS_allegro_get_font(SMALL), LS_allegro_get_color(text_color), WIN_WIDTH/5*3.2, WIN_HEIGHT/5*3 + 100, 0, "%s%s", "AERO REAR: ", "HIGH LOAD");
+                    al_draw_textf(LS_allegro_get_font(SMALL), LS_allegro_get_color(text_color), WIN_WIDTH/5*3.2, WIN_HEIGHT/5*3 + 120, 0, "%s%s", "FUEL: ", "REGULAR");
+                    al_draw_textf(LS_allegro_get_font(SMALL), LS_allegro_get_color(text_color), WIN_WIDTH/5*3.2, WIN_HEIGHT/5*3 + 140, 0, "%s%s", "ENGINE: ", "SPEC C");
+
+
+
+                    //'Painting' the graphic screen.
+                    LS_allegro_clear_and_paint(background_color);
                 }
 
                 //Tanquem la finestra gràfica
